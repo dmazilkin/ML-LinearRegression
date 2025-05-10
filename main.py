72669429f956ca9469d6ab3d21932d3702afdfd7
Dmitrii Mazilkin <dim.mazilkin@gmail.com>
2025-05-10 09:44:59 +0200

    Add fit method, update main.

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 from src.linreg import LinearRegression
 
 def main():
     model = LinearRegression(epochs=1000, lr=0.01)
     print(model)
+
+    X_data = np.random.rand(100, 1) * 10
+    X_real = np.arange(X_data.min(), X_data.max(), step=0.1)
+    X_real = X_real.reshape(X_real.shape[0], 1)
+
+    y_data = 3 * X_data + 5 + np.random.randn(100, 1) * 2
+    y_real = 3 * X_real + 5
+
+    model.fit(X_data, y_data, verbose=10)
+    weights = model.get_coef()
+    print(X_real.shape, weights.shape)
+    prediction = np.sum(model._weights.T * np.concatenate([np.ones([X_real.shape[0], 1]), X_real], axis=1), axis=1)
+
+    plt.plot(X_real, y_real, c='g', marker='_')
+    plt.scatter(X_data, y_data, c='b', marker='*')
+    plt.plot(X_real, prediction, c='r', marker='+')
+    plt.show()
 
 if __name__ == '__main__':
     main()
--- src/linreg.py
+++ src/linreg.py
@@ -1,7 +1,36 @@
+import pandas as pd
+import numpy as np
+from typing import Union
+
 class LinearRegression:
     def __init__(self, epochs: int = 100, lr: float = 0.1) -> None:
         self._epochs = epochs
         self._lr = lr
+        self._weights = None
+
+    def fit(self, X: pd.DataFrame, y: pd.Series, verbose: Union[bool, int] = False):
+        N = X.shape[0]
+        X = np.concatenate([np.ones([N, 1]), X], axis=1)
+        y = y.reshape(N, 1)
+
+        self._weights = np.ones([X.shape[1], 1])
+
+        for epoch in range(self._epochs):
+            y_pred = X @ self._weights
+            error = y - y_pred
+            grad = (-1) * 2 * (X.T @ error) / N
+            mse = 1 / N * np.sum(error**2)
+
+            self._weights -= self._lr * grad
+
+            if verbose:
+                if epoch == 0:
+                    print(f'start | loss: {mse}')
+                elif epoch % verbose == 0:
+                    print(f'{epoch} | loss: {mse}')
+
+    def get_coef(self):
+        return self._weights
 
     def __str__(self):
         representation = 'LinearRegression: '
