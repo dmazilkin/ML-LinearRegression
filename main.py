549180bb84e84435f07646e4502109a835b6f1dc
Dmitrii Mazilkin <dim.mazilkin@gmail.com>
2025-05-10 10:24:44 +0200

    Add predict method, formatting.

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import matplotlib.pyplot as plt
 from src.linreg import LinearRegression
 
 def main():
-    model = LinearRegression(epochs=1000, lr=0.01)
-    print(model)
 
     X_data = np.random.rand(100, 1) * 10
     X_real = np.arange(X_data.min(), X_data.max(), step=0.1)
@@ -15,10 +13,9 @@ def main():
     y_data = 3 * X_data + 5 + np.random.randn(100, 1) * 2
     y_real = 3 * X_real + 5
 
-    model.fit(X_data, y_data, verbose=10)
-    weights = model.get_coef()
-    print(X_real.shape, weights.shape)
-    prediction = np.sum(model._weights.T * np.concatenate([np.ones([X_real.shape[0], 1]), X_real], axis=1), axis=1)
+    model = LinearRegression(epochs=1000, lr=0.01)
+    model.fit(pd.DataFrame(X_data), pd.Series(y_data.reshape(-1)), verbose=10)
+    prediction = model.predict(pd.DataFrame(X_real)).to_numpy()
 
     plt.plot(X_real, y_real, c='g', marker='_')
     plt.scatter(X_data, y_data, c='b', marker='*')
--- src/linreg.py
+++ src/linreg.py
@@ -3,24 +3,32 @@ import numpy as np
 from typing import Union
 
 class LinearRegression:
+
     def __init__(self, epochs: int = 100, lr: float = 0.1) -> None:
         self._epochs = epochs
         self._lr = lr
         self._weights = None
 
     def fit(self, X: pd.DataFrame, y: pd.Series, verbose: Union[bool, int] = False):
+        # pandas to numpy
+        X = X.to_numpy()
+        y = y.to_numpy()
+        # preprocess data: add bias to features, reshape target vector
         N = X.shape[0]
         X = np.concatenate([np.ones([N, 1]), X], axis=1)
         y = y.reshape(N, 1)
-
+        # initialize weights as ones vector
         self._weights = np.ones([X.shape[1], 1])
 
         for epoch in range(self._epochs):
+            # calculate prediction
             y_pred = X @ self._weights
+            # calculate error
             error = y - y_pred
+            # calculate mse and gradient for MSE
+            mse = 1 / N * np.sum(error ** 2)
             grad = (-1) * 2 * (X.T @ error) / N
-            mse = 1 / N * np.sum(error**2)
-
+            # update weights with gradient descent
             self._weights -= self._lr * grad
 
             if verbose:
@@ -28,6 +36,17 @@ class LinearRegression:
                     print(f'start | loss: {mse}')
                 elif epoch % verbose == 0:
                     print(f'{epoch} | loss: {mse}')
+
+    def predict(self, X: pd.DataFrame) -> pd.Series:
+        # preprocess data
+        X = X.to_numpy()
+        N = X.shape[0]
+        X = np.concatenate([np.ones([N, 1]), X], axis=1)
+        # calculate prediction
+        prediction = X @ self._weights
+
+        return pd.Series(prediction.reshape(-1))
+
 
     def get_coef(self):
         return self._weights
